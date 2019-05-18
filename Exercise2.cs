fe5badd65d34ca0b9934e4e8d980ee2e39406758
SunchiWHU <804806324@qq.com>
2019-05-18 18:52:34 +0800

    2

---
+++ Exercise2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 101;//素数的开始
+            int count = 0;//素数的个数
+            for (i = 101; i <= 200; i++)
+            {
+                int j = 0;
+                for (j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        break;
+                    }
+                }
+                if (j == i)
+                {
+                    count++;
+                    Console.WriteLine(i);
+                }
+            }
+           
+        }
+    }
+}
