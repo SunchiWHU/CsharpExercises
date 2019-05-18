b2b9a41f5a43edcd9345b8e40d7979d25e4ed7b2
SunchiWHU <804806324@qq.com>
2019-05-18 18:59:04 +0800

    5

---
+++ Exercise5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("线程实现5秒休眠");
+            Thread t = new Thread(PrintNumbers);
+            Thread.Sleep(5000);
+            Console.WriteLine("休眠结束，事件开启");
+            t.Start();
+
+        }
+        static void PrintNumbers()
+        {
+
+            Console.WriteLine("{0}Starting...",DateTime.Now.ToLongDateString());
+           
+        }
+    }
+}
